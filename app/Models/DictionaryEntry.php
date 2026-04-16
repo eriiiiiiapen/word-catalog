1cb09ca41e692074285271018c626e745c28824a
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-04-16 21:31:43 +0900

    DictionaryEntry：model/migration追加

---
+++ app/Models/DictionaryEntry.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DictionaryEntry extends Model
+{
+    protected $fillable = [
+        'table_name',
+        'physical_name',
+        'logical_name',
+        'description',
+        'public_token',
+        'is_published',
+    ]
+}

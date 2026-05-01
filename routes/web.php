5c1d4961df415db2ed29b9ea8e6f36ed18a33925
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-05-02 02:47:48 +0900

    インポート画面へのリンクボタンを追加

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Volt::route('/import-sql-with-project', 'dictionary.import-sql-with-project');
-Volt::route('/import-sql', 'dictionary.import-sql');
-Volt::route('/', 'dictionary.index');
+Volt::route('/import-sql-with-project', 'dictionary.import-sql-with-project')->name('dictionary.import-sql-with-project');
+Volt::route('/import-sql', 'dictionary.import-sql')->name('dictionary.import-sql');
+Volt::route('/', 'dictionary.index')->name('dictionary.index');
